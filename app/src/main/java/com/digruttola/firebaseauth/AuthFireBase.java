5d065a05cebfd045c703c5d1f74fb389269d21de
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-06-04 15:08:41 -0300

    Agregando la libreria FirebaseAuth

---
+++ app/src/main/java/com/digruttola/firebaseauth/AuthFireBase.java
@@ -0,0 +1,8 @@
+package com.digruttola.firebaseauth;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AuthFireBase {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+}
